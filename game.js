6fd0e47fe054e29eaad4ccafd7695ac1b4669358
bhanna1693 <bhanna1693@gmail.com>
2018-12-13 09:31:57 -0500

    updated js

--- game.js
+++ game.js
@@ -9,22 +9,17 @@
     let resetButton = document.querySelector("#reset");
     let resetCount = document.querySelector("#num-resets");
     let audio = document.querySelector("#horn");
-
-
     
 
     team1ShotButton.addEventListener("click", function(){
         let shot = Math.random();
         team1Shots.innerHTML = parseInt(team1Shots.innerHTML) + 1;
-        let goals = parseInt(team1Goals.innerHTML);
-
         
         if(shot < 0.45){
             document.body.style.backgroundColor = "white";
             console.log("miss");
         } else {
-            goals += 1;
-            team1Goals.innerHTML = goals;
+            team1Goals.innerHTML = parseInt(team1Goals.innerHTML) + 1;
             document.body.style.backgroundColor = "yellow";
             audio.play();
             console.log("GOALLLLL");
@@ -34,15 +29,12 @@
     team2ShotButton.addEventListener("click", function(){
         let shot = Math.random();
         team2Shots.innerHTML = parseInt(team2Shots.innerHTML) + 1;
-        let goals = parseInt(team2Goals.innerHTML);
-
         
         if(shot < 0.55){
             document.body.style.backgroundColor = "white";
             console.log("miss");
         } else {
-            goals += 1;
-            team2Goals.innerHTML = goals;
+            team2Goals.innerHTML = parseInt(team2Goals.innerHTML) + 1;
             document.body.style.backgroundColor = "red";
             audio.play();
             console.log("GOALLLLL");
